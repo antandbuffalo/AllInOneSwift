c354373e72ee68dc51c17cd7644ff3f64f494778
Jeyabalaji <balaji.jeya@gmail.com>
2017-10-26 21:49:25 +0800

    table added

--- AllInOneSwift/ViewController.swift
+++ AllInOneSwift/ViewController.swift
@@ -8,7 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var myData: Array<String> = [];
+    var inProgress: Bool = false;
+    
+    @IBOutlet weak var myTable: UITableView!
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let actualPosition: CGFloat = scrollView.contentOffset.y;
+        let contentHeight: CGFloat = scrollView.contentSize.height - 1000;
+        print("actual \(actualPosition) -- content - \(contentHeight)");
+        if (actualPosition < 100) {
+            inProgress = true;
+            _ = Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { (timer) in
+                if(self.inProgress) {
+                    self.inProgress = false;
+                    self.myData.insert("a", at: 0);
+                    self.myData.insert("b", at: 0);
+                    self.myTable.reloadData();
+                    let indexPath = IndexPath(row: 1, section: 0);
+                    self.myTable.scrollToRow(at: indexPath, at: .top, animated: true);
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myData.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell")!;
+        cell.textLabel?.text = myData[indexPath.row];
+        return cell;
+    }
+    
     
     func readDataFromPlist() {
         var name: String;
@@ -38,8 +73,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTable.dataSource = self;
+        myTable.delegate = self;
         
         readDataFromPlist();
+        
+        for i in 1...20 {
+            myData.append(String(i));
+        }
+        print(myData);
+        myTable.reloadData();
+        let indexPath = IndexPath(row: myData.count - 1, section: 0);
+        self.myTable.scrollToRow(at: indexPath, at: .bottom, animated: true);
     }
 
     override func didReceiveMemoryWarning() {
